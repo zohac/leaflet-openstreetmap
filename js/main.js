76f0a0ee3319c84303efb4a2f7ebb7e8dc52c2ea
zohac <fenrir0680@gmail.com>
2019-09-15 16:09:58 +0200

    update the project.

--- js/main.js
+++ js/main.js
@@ -12,19 +12,33 @@
     // The DOM is ready!
 
     const regions = {
-      "auvergne-rhone-alpes": "Auvergne-Rhône-Alpes",
-      "bourgogne-franche-comte": "Bourgogne-Franche-Comté",
-      "bretagne": "Bretagne",
-      "centre-val-de-loire": "Centre-Val de Loire",
-      "corse": "Corse",
-      "grand-est": "Grand Est",
-      "hauts-de-france": "Hauts-de-France",
-      "ile-de-france": "Île-de-France",
-      "normandie": "Normandie",
-      "nouvelle-aquitaine": "Nouvelle-Aquitaine",
-      "occitanie": "Occitanie",
-      "pays-de-la-loire": "Pays de la Loire",
-      "provence-alpes-cote-azur": "Provence-Alpes-Côte d'Azur",
+      "auvergne-rhone-alpes.min": "Auvergne-Rhône-Alpes",
+      "bourgogne-franche-comte.min": "Bourgogne-Franche-Comté",
+      "bretagne.min": "Bretagne",
+      "centre-val-de-loire.min": "Centre-Val de Loire",
+      "grand-est.min": "Grand Est",
+      "hauts-de-france.min": "Hauts-de-France",
+      "ile-de-france.min": "Île-de-France",
+      "normandie.min": "Normandie",
+      "nouvelle-aquitaine.min": "Nouvelle-Aquitaine",
+      "occitanie.min": "Occitanie",
+      "pays-de-la-loire.min": "Pays de la Loire",
+      "provence-alpes-cote-azur.min": "Provence-Alpes-Côte d'Azur",
+      "corse.min": "Corse",
+      // "auvergne-rhone-alpes": "Auvergne-Rhône-Alpes",
+      // "bourgogne-franche-comte": "Bourgogne-Franche-Comté",
+      // "bretagne": "Bretagne",
+      // "centre-val-de-loire": "Centre-Val de Loire",
+      // "grand-est": "Grand Est",
+      // "hauts-de-france": "Hauts-de-France",
+      // "ile-de-france": "Île-de-France",
+      // "normandie": "Normandie",
+      // "nouvelle-aquitaine": "Nouvelle-Aquitaine",
+      // "occitanie": "Occitanie",
+      // "pays-de-la-loire": "Pays de la Loire",
+      // "provence-alpes-cote-azur": "Provence-Alpes-Côte d'Azur",
+      // "corse": "Corse",
+      // "regions": "regions",
     };
 
     const map = L.map('map').setView([46.866, 0.242], 6);
@@ -50,7 +64,40 @@
           }
         });
         mapLayer.addTo(map);
+        mapLayer.setStyle({
+          color: '#61ab27',
+          fillOpacity: 0.3
+        });
+        mapLayer.on('mouseover', function highlightFeature(e) {
+          let layer = e.target;
 
+          layer.setStyle({
+            color: '#03a9f4',
+            fillOpacity: 0.7
+          });
+
+          if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+            layer.bringToFront();
+          }
+        });
+        mapLayer.on('mouseout', function highlightFeature(e) {
+          let layer = e.target;
+
+          layer.setStyle({
+            color: '#61ab27',
+            fillOpacity: 0.3
+          });
+
+          if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+            layer.bringToFront();
+          }
+        });
+
+        mapLayer.on('click', function(e){
+          let layer = e.target;
+
+          console.log(value);
+        });
         overlaysMaps[`Région ${value}`] = mapLayer;
       }).promise();
       requestQueue.push(request);
@@ -58,7 +105,6 @@
 
     Promise.all(requestQueue).then(() => {
       // all requests finished successfully
-      console.log(overlaysMaps);
       L.control.layers(baseMap, overlaysMaps, {collapsed: false}).addTo(map);
       L.control.scale({imperial: false}).addTo(map);
     }).catch(() => {
